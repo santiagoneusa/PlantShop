849b75f8ef30aef15a53abbe0b0b476ffc55431a
santiagoneusa <sneusar@gmail.com>
2024-03-12 16:27:14 -0500

    Implement admin home page

--- routes/web.php
+++ routes/web.php
@@ -13,12 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
 Auth::routes();
 
 Route::get('/', 'App\Http\Controllers\HomeController@index')->name('home.index');
+
 Route::get('/plants', 'App\Http\Controllers\PlantController@index')->name('plant.index');
 Route::get('/plants/create', 'App\Http\Controllers\PlantController@create')->name('plant.create');
 Route::post('/plants/save', 'App\Http\Controllers\PlantController@save')->name('plant.save');
 Route::get('/plants/{id}', 'App\Http\Controllers\PlantController@show')->name('plant.show');
 Route::delete('/plants/{id}', 'App\Http\Controllers\PlantController@delete')->name('plant.delete');
+
+Route::get('/admin', 'App\Http\Controllers\Admin\AdminHomeController@index')->name("admin.home.index");
