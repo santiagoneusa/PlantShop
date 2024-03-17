64bf226438eaa4105b9e6339becfc5cae6499653
santiagoneusa <sneusar@gmail.com>
2024-03-17 12:10:37 -0500

    Fix plant views on user and admin

--- resources/views/admin/plant/show.blade.php
+++ resources/views/admin/plant/show.blade.php
@@ -2,9 +2,27 @@
 @section('title', $viewData["title"])
 @section('content')
 <div class="card-header">
-    <h1 class="ms-5">Admin Panel</h1>
+    <h1 class="ms-5">{{ $viewData["plant"]->getName() }} Information</h1>
 </div>
 <div class="m-3 row">
-    
+    <div class="col-md-4">
+      <img src="{{ asset('/storage/'.$viewData["plant"]->getImage()) }}" class="img-fluid rounded-start">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">{{ $viewData["plant"]->getName() }}</h5>
+        <p class="card-text">ID: {{ $viewData["plant"]->getId() }}</p>
+        <p class="card-text">Description: {{ $viewData["plant"]->getDescription() }}</p>
+        <p class="card-text">Price: ${{ $viewData["plant"]->getPrice() }}</p>
+        <p class="card-text">Remaining Stock: {{ $viewData["plant"]->getStock() }} units</p>
+      </div>
+    </div>
+    <div class="d-flex justify-content-center mt-4">  <form action="{{ route('admin.plant.edit', ['id' => $viewData["plant"]->getId()]) }}" method="post" enctype="multipart/form-data">
+            <button class="btn btn-primary me-2">Edit</button>  </form>
+        <form action="{{ route('admin.plant.delete', ['id' => $viewData["plant"]->getId()]) }}" method="post" enctype="multipart/form-data">
+            <button class="btn btn-danger">Delete</button>
+        </form>
+    </div>
 </div>
+
 @endsection
