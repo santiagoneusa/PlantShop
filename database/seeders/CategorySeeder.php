04ab096dfb3931a42432a349d14ebec9ce033d4c
Jhonnathan93 <jsocampod@eafit.edu.co>
2024-05-19 11:47:18 -0500

    Modify dockerfile

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -9,12 +9,8 @@ class CategorySeeder extends Seeder
 {
     public function run(): void
     {
-        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
-
         Category::whereIn('name', ['Ornamental', 'Indoor', 'Outdoor', 'Aromatic'])->delete();
 
-        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
-        
         $categories = [
             [
                 'id' => 1,
