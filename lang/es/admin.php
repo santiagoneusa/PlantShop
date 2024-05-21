ff51650ad840b48e01c543dfc49b74c78dd9553a
santiagoneusa <sneusar@gmail.com>
2024-05-21 00:22:19 -0500

    implement lang in spanish

---
+++ lang/es/admin.php
@@ -0,0 +1,73 @@
+<?php
+
+return [
+
+    // General
+
+    'admin_panel' => 'Panel de Administración',
+    'admin' => 'Administrador',
+    'go_to_store' => 'Ir a la Tienda',
+    
+    'home' => 'Inicio',
+    'plants' => 'Plantas',
+    'guides' => 'Guías',
+    
+    'default_card_text_plants' => 'Crear, eliminar o editar las plantas de la base de datos',
+    'default_card_text_guides' => 'Crear, eliminar o editar las guías de la base de datos',
+    
+    'manage_guides' => 'Administrar Guías',
+    'manage_plants' => 'Administrar Plantas',
+
+    // Guides
+
+    'table_header_guide_id' => 'ID',
+    'table_header_guide_title' => 'Título',
+    'table_header_guide_content' => 'Contenido',
+    'table_header_guide_edit' => 'Editar',
+    'table_header_guide_delete' => 'Eliminar',
+
+    'hyphen_formatted_guide_title' => '":title" - Información',
+    'colon_formatted_guide_title' => 'Título: :title',
+    'colon_formatted_guide_id' => 'ID: :id',
+    'colon_formatted_guide_content' => 'Contenido: :content',
+
+    'button_guide_create' => 'Crear',
+    'button_guide_update' => 'Actualizar',
+    'button_guide_edit' => 'Editar',
+    'button_guide_cancel' => 'Cancelar',
+    'button_guide_delete' => 'Eliminar',
+
+    // Plants
+
+    'table_header_plant_id' => 'ID',
+    'table_header_plant_name' => 'Nombre',
+    'table_header_plant_description' => 'Descripción',
+    'table_header_plant_price' => 'Precio',
+    'table_header_plant_stock' => 'Inventario',
+    'table_header_plant_edit' => 'Editar',
+    'table_header_plant_delete' => 'Eliminar',
+    
+    'button_plant_create' => 'Crear',
+    'button_plant_update' => 'Actualizar',
+    'button_plant_edit' => 'Editar',
+    'button_plant_cancel' => 'Cancelar',
+    'button_plant_delete' => 'Eliminar',
+    
+    'hyphen_formatted_plant_title' => '":title" - Información',
+    'colon_formatted_plant_edditing' => 'Editando :plant',
+    'colon_formatted_plant_current_category' => 'Valor actual: :category',
+    'colon_formatted_plant_name' => 'Nombre: :name',
+    'colon_formatted_plant_id' => 'ID: :id',
+    'colon_formatted_plant_description' => 'Descripción: :description',
+    'colon_formatted_plant_price' => 'Precio: $:price',
+    'colon_formatted_plant_stock' => 'Inventario restante: :stock unidades',
+    'colon_formatted_plant_category' => 'Categoría: :category',
+    
+    'select_plant_category' => 'Seleccionar Categoría',
+
+    // Partials
+
+    'copyright' => '© 2024 Derechos de Autor - ',
+    'names' => 'Jhonnathan Ocampo y Santiago Neusa',
+
+];
---
+++ lang/es/app.php
@@ -0,0 +1,120 @@
+<?php
+
+return [
+
+    // Libro
+
+    'books' => 'Libros',
+
+    'card_book_description' => '¿Quieres saber más sobre cómo cuidar tus plantas? Te dejamos una lista de libros que podrían ser útiles para esto. Anímate y busca más formas de cuidar tus plantas',
+    'card_book_autor' => 'Autor: ',
+    'card_book_year' => 'Año: ',
+    'card_book_language' => 'Idioma: ',
+    
+    'no_books_found' => 'No se encontraron libros.',
+    
+    // Carrito
+
+    'plants_in_cart' => 'Plantas en el carrito',
+    'total_to_pay' => 'Total a pagar: $',
+    'not_enough_balance' => 'Saldo insuficiente',
+    'remove_all_plants_from_cart' => 'Eliminar todas las plantas del carrito',
+    'purchase_completed' => 'Compra completada',
+    'purchase_completed_message' => 'Felicidades, compra completada. El número de pedido es #',
+    'address' => 'Dirección',
+
+    'table_header_cart_id' => 'ID',
+    'table_header_cart_name' => 'Nombre',
+    'table_header_cart_price' => 'Precio',
+    'table_header_cart_quantity' => 'Cantidad',
+    
+    // Categoría
+    
+    'category' => 'Categoría',
+    'categories' => 'Categorías',
+    'view_categories' => 'Ver Categorías',
+    'card_categories_available' => 'Visualiza plantas organizadas por diferentes tipos de luz, función y entorno',
+    'more_details' => 'Más detalles',
+    'no_plants_for_this_category' => 'No hay plantas para esta categoría',
+
+    'ornamental' => 'Ornamental',
+    'indoor' => 'Interior',
+    'outdoor' => 'Exterior',
+    'aromatic' => 'Aromáticas',
+
+    // Guía
+
+    'guides' => 'Guías',
+    'view_guides' => 'Ver Guías',
+    
+    'card_guides_available' => '¿Cómo regar mis plantas? ¿Cuáles plantas son mejores para mí? Todas esas preguntas respondidas aquí',
+    'colon_formatted_guide_description' => 'Descripción: :description',
+
+    // Inicio
+
+    'home' => 'Inicio',
+    'button_search' => 'Buscar',
+    'allied_store' => 'Tienda Aliada',
+    'search' => 'Buscar',
+    'search_plants' => 'Buscar plantas...',
+    
+    // Planta
+
+    'plants' => 'Plantas',
+    'view_plants' => 'Ver Plantas',
+    'card_plants_available' => 'Visualiza todas las plantas disponibles',
+        
+    'select_plants_filter' => 'Selecciona una opción para filtrar las plantas',
+    'select_newest_to_oldest' => 'De más nuevo a más antiguo',
+    'select_oldest_to_newest' => 'De más antiguo a más nuevo',
+    'select_price_higher_to_lower' => 'Precio: de mayor a menor',
+    'select_price_lower_to_higher' => 'Precio: de menor a mayor',
+    'apply_filter' => 'Aplicar filtro',
+
+    'colon_formatted_plant_name' => 'Nombre: :name',
+    'colon_formatted_plant_id' => 'ID: :id',
+    'colon_formatted_plant_description' => 'Descripción: :description',
+    'colon_formatted_plant_price' => 'Precio: $:price',
+    'colon_formatted_plant_stock' => 'Stock restante: :stock unidades',
+    'colon_formatted_plant_category' => 'Categoría: :category',
+
+    // Comentarios
+
+    'comments' => 'Comentarios',
+    'add_comment' => 'Agregar comentario',
+    'button_comment_send' => 'Enviar comentario',
+    'comments_login' => 'Debes iniciar sesión para comentar y ver comentarios.',
+    'rating' => '¿Cómo calificarías este producto en una escala de uno a cinco?',
+    'stars' => 'Estrellas: :stars',
+
+    // Productos
+
+    'colon_formatted_product_id' => 'ID: :id',
+    'colon_formatted_product_description' => 'Descripción: :description',
+    'colon_formatted_product_price' => 'Precio: $:price',
+    'colon_formatted_product_stock' => 'Stock restante: :stock unidades',
+    'colon_formatted_product_category' => 'Categoría: :category',
+
+    'product_link' => 'Enlace del producto',
+
+    // Usuario
+
+    'colon_formatted_user_name' => 'ID: :id',
+    'colon_formatted_user_balance' => 'Saldo: $:balance',
+    'colon_formatted_user_email' => 'Correo electrónico: :email',
+    'colon_formatted_user_since' => 'Desde: :date',
+    
+    'not_purchase' => 'Parece que no has comprado nada en nuestra tienda.',
+
+    // Pedido
+
+    'hash_formatted_order_id' => 'Pedido #:id',
+    'colon_formatted_order_date' => 'Fecha: :date',
+    'colon_formatted_order_total' => 'Total: $:total',
+
+    'table_header_product_item_id' => 'ID del ítem',
+    'table_header_product_name' => 'Nombre del producto',
+    'table_header_product_price' => 'Precio',
+    'table_header_product_quantity' => 'Cantidad',
+
+];
